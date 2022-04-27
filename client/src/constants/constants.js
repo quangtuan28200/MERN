d88dc93df94a13fa8401f2390331f2feb411fa03
Bùi Quang Tuấn <74997976+quangtuan28200@users.noreply.github.com>
2022-04-27 23:59:14 +0700

    done1

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "https://immense-ravine-04867.herokuapp.com/api"
+    ? "http://localhost:5000/api"
     : "https://immense-ravine-04867.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mern";
