6bee7a48a63f235e6c1776988784201350b260ae
Bùi Quang Tuấn <74997976+quangtuan28200@users.noreply.github.com>
2022-04-27 23:10:00 +0700

    deploy

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -1,21 +1,21 @@
-import Button from 'react-bootstrap/Button'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const About = () => {
-	return (
-		<Row className='mt-5' style={{ marginRight: 0 }}>
-			<Col className='text-center'>
-				<Button
-					variant='primary'
-					href='https://www.youtube.com/c/HenryWebDev'
-					size='lg'
-				>
-					Visit my channel for more tutorials
-				</Button>
-			</Col>
-		</Row>
-	)
-}
+  return (
+    <Row className="mt-5" style={{ marginRight: 0 }}>
+      <Col className="text-center">
+        <Button
+          variant="primary"
+          href="https://quangtuan28200.github.io/myCV"
+          size="lg"
+        >
+          Visit my CV
+        </Button>
+      </Col>
+    </Row>
+  );
+};
 
-export default About
+export default About;
--- server/index.js
+++ server/index.js
@@ -27,5 +27,5 @@ app.use(cors());
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("server started on port 5000"));
